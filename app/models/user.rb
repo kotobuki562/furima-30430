cc176e5770dd21e93f0db1036aeaaeceb29f6619
MaQueen <shusineko@icloud.com>
2020-09-29 21:32:28 +0900

    表記制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
