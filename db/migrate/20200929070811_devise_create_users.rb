4e12576ff9ce838f11f166b7ca2e1fe6f5181ea8
MaQueen <shusineko@icloud.com>
2020-09-29 16:34:48 +0900

    モデルとマグレーションファイルへカラムを記述

--- db/migrate/20200929070811_devise_create_users.rb
+++ db/migrate/20200929070811_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname
+      t.string :family_name
+      t.string :first_name
+      t.string :family_name_furigana
+      t.string :first_name_furigana
+      t.date :birthday
 
       ## Recoverable
       t.string   :reset_password_token
