3c1120b60ce250e996473b7c94285d4a360c6332
MaQueen <shusineko@icloud.com>
2020-10-01 18:35:18 +0900

    出品機能の実装（jsファイルは未作成）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
+    # @items = Item.includes(:user)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :text, :name, :category_id, :state_id, :delivery_charge_id, :shipping_place_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :state
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :shipping_place
+  belongs_to_active_hash :shipping_date
+
+  VALID_PRICE_REGEX = /[0-9][\d]\w/.freeze
 
   with_options presence: true do
     validates :image
-    validates :name
-    validates :text
-    validates :category_id
-    validates :state_id
-    validates :delivery_charge_id
-    validates :shipping_place_id
-    validates :shipping_date_id
-    validates :price
+    validates :name,length: {maximum: 40}
+    validates :text,length: {maximum: 1000}
+    validates :category_id,numericality: { other_than: 1 }
+    validates :state_id,numericality: { other_than: 1 }
+    validates :delivery_charge_id,numericality: { other_than: 1 }
+    validates :shipping_place_id,numericality: { other_than: 1 }
+    validates :shipping_date_id,numericality: { other_than: 1 }
+
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 999999999 },
+    format: { with: VALID_PRICE_REGEX }
 
   end
 
