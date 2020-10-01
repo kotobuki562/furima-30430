9cc453cf4009b94d113d5e59e515cfab6193aa9c
MaQueen <shusineko@icloud.com>
2020-10-01 16:54:07 +0900

    active_hashの値設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :category_id
+    validates :state_id
+    validates :delivery_charge_id
+    validates :shipping_place_id
+    validates :shipping_date_id
+    validates :price
+
+  end
+
   belongs_to :user
   has_one_attached :image
 end
