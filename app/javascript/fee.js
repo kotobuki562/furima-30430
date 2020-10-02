2da440b76a528bb94559fdc28421046354ffca39
MaQueen <shusineko@icloud.com>
2020-10-02 14:13:41 +0900

    js処理をfee.jsに集約

--- app/javascript/fee.js
+++ app/javascript/fee.js
@@ -4,6 +4,8 @@ function fee() {
     const itemPriceCount = itemPrice.value;
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${itemPriceCount * 0.1}`;
+    const profitTotal = document.getElementById("profit");
+    profitTotal.innerHTML = `${itemPriceCount * 0.9}`;
   });
 }
 window.addEventListener("load", fee);
