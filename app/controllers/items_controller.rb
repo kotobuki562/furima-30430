245810e43358a73dd65c34a1e1ada7ab54ac7fa5
MaQueen <shusineko@icloud.com>
2020-10-04 14:49:26 +0900

    destroyアクションにエラーハンドリング実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,8 +41,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+    else
+      render :show
+    end
+
   end
 
   private
