4b016adde74904883ebda0d62f99d00ab8d580f7
MaQueen <shusineko@icloud.com>
2020-10-05 17:56:16 +0900

    payjpのアクション設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,4 +4,19 @@ class PurchasesController < ApplicationController
 
   def create
   end
+
+  private
+
+  def purchase_params
+    params.require(:address_purchase).permit(:token,:post_coade,:prefectures_id,:municipality,:address,:tellphone_number,:building)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      # amount: order_params[:price],
+      card: purchase_params[:token],
+      currency:'jpy'
+    )
+  end
 end
